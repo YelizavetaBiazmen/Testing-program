f51947901853a91e61c55acc9cc7e41d45a5fecf
YelizavetaBiazmen <95139780+YelizavetaBiazmen@users.noreply.github.com>
2023-05-07 15:42:14 +0200

    Update testing program.py

--- testing program.py
+++ testing program.py
@@ -12,28 +12,28 @@ correctAns = 0 #deklarujemy zmienną, która mówi o ilości prawidłowych odpow
 wrongAns = 0 #deklarujemy zmienną, która mówi o ilości nieprawidłowych odpowiedzi
 counter = 0 #deklarujemy zmienną, zmienia ilość wszystkich pytań
 for element in qstns:
-    counter += 1
-    print(Fore.YELLOW + 'Question:', element['qestion'], Style.RESET_ALL + ' ' )
+    counter += 1 
+    print(Fore.YELLOW + 'Question:', element['qestion'], Style.RESET_ALL + ' ' ) #kolorujemy tekst
     print( 'Please, choose the correct answer.')
     a = 0
     correctAnsversString = element['answers'][element['correctAnswer'] - 1]
-    qstnsQty += 1
-    random.shuffle(element['answers'])
+    qstnsQty += 1 #dodajemy pytanie 
+    random.shuffle(element['answers']) #przetasujemy odpowiedzi w tablicy
     for answer in element['answers']:
         a +=  1
-        print(Fore.BLUE + ' ',a, '.', Style.RESET_ALL + ' ' , answer)
-    print('What answer is right?')
-    userAns = ''
-    while not(userAns == '1' or userAns == '2' or userAns == '3' or userAns == '4'):
+        print(Fore.BLUE + ' ',a, '.', Style.RESET_ALL + ' ' , answer) #kolorujemy tekst
+    print('What answer is right?') 
+    userAns = '' #deklarujemy zmienną, do której potem wpiszemy odpowiedz użytkownika
+    while not(userAns == '1' or userAns == '2' or userAns == '3' or userAns == '4'): #zmuszamy napisać 1, 2, 3 czy 4
         print('enter 1, 2, 3 or 4.')
-        userAns = input()
+        userAns = input() #przenosimy odpowiedz do zmiennej
     userAnsStr =  element['answers'][ int(userAns)- 1]
-    if  correctAnsversString ==  userAnsStr:
-        print( Fore.GREEN + 'Right!', Style.RESET_ALL + ' ')
-        correctAns += 1
+    if  correctAnsversString ==  userAnsStr: #sprawdzamy, czy jest odpowiedz poprawna 
+        print( Fore.GREEN + 'Right!', Style.RESET_ALL + ' ') #kolorujemy tekst
+        correctAns += 1 #dodajemy poprawną odpowiedz
     else:
-        print(Fore.RED + 'No.' , Style.RESET_ALL + 'the correct answer is', correctAnsversString)
-        wrongAns += 1
+        print(Fore.RED + 'No.' , Style.RESET_ALL + 'the correct answer is', correctAnsversString) #kolorujemy tekst
+        wrongAns += 1 #dodajemy niepoprawną odpowiedz
     if counter == 30: #ustaliamy po jakiej ilości zadanych pytań pokażą się wyniki
         break
 pers = (correctAns / qstnsQty) * 100  #procent poprawnych odpowiedzi  
