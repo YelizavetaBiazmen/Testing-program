6e833959bf155bf3effe57abea359eb3a9afe6b6
YelizavetaBiazmen <95139780+YelizavetaBiazmen@users.noreply.github.com>
2023-05-13 19:44:18 +0200

    Update testing program.py

--- testing program.py
+++ testing program.py
@@ -8,34 +8,42 @@ with open('data/qestions.json') as file: #lączymy programę z pytaniami json
     qstns = json.load(file)
 random.shuffle(qstns) #przetasujemy pytania w tablicy
 #deklarujemy zmienne
-qstnsQty = 0 #mówi o ilości zadanych pytań
-correctAns = 0 #mówi o ilości prawidłowych odpowiedzi
-wrongAns = 0 #mówi o ilości nieprawidłowych odpowiedzi
-counter = 0 #zmienia ilość wszystkich pytań
+currentMaxQtyQstns = 30 #maksymalna ilość pytań
+qstnsQty = 0 #ilość zadanych pytań
+correctAns = 0 #ilość prawidłowych odpowiedzi
+wrongAns = 0 #ilość nieprawidłowych odpowiedzi
+counter = 0 #zawiera liczbę już zadanych pytań
+
+#cykl dla zadawania pytań
 for element in qstns:
     counter += 1 
-    print(Fore.YELLOW + 'Question:', element['qestion'], Style.RESET_ALL + ' ' ) #kolorujemy tekst
+    print(Fore.YELLOW + 'Question:', element['qestion'], Style.RESET_ALL + ' ' ) 
     print( 'Please, choose the correct answer.')
-    a = 0
-    correctAnsversString = element['answers'][element['correctAnswer'] - 1] #odzyskujemy i wpisujemy do zmiennej poprawną odpowiedz z tablicy, odejmując 1, ponieważ numeracja w tablicy zaczyna się od 0
-    qstnsQty += 1 #dodajemy pytanie 
+    numberOfAns = 0 #deklarujemy zmienną, która numeruje odpowiedzi
+    
+    #odzyskujemy i wpisujemy do zmiennej poprawną odpowiedz z tablicy, odejmując 1, ponieważ numeracja w tablicy zaczyna się od 0
+    correctAnsversString = element['answers'][element['correctAnswer'] - 1] 
     random.shuffle(element['answers']) #przetasujemy odpowiedzi w tablicy
+    
+    #cykl dla odwowiedzi
     for answer in element['answers']:
-        a +=  1
-        print(Fore.BLUE + ' ',a, '.', Style.RESET_ALL + ' ' , answer) #kolorujemy tekst
+        numberOfAns +=  1
+        print(Fore.BLUE + ' ',numberOfAns, '.', Style.RESET_ALL + ' ' , answer) 
     print('What answer is right?') 
     userAns = '' #deklarujemy zmienną, do której potem wpiszemy odpowiedz użytkownika
-    while not(userAns == '1' or userAns == '2' or userAns == '3' or userAns == '4'): #zmuszamy napisać 1, 2, 3 czy 4
+    
+    #czekamy aż użytkownik napisze jedną z możliwych odpowiedzi
+    while not(userAns == '1' or userAns == '2' or userAns == '3' or userAns == '4'): 
         print('enter 1, 2, 3 or 4.')
         userAns = input() #przenosimy odpowiedz do zmiennej
     userAnsStr =  element['answers'][ int(userAns)- 1]
     if  correctAnsversString ==  userAnsStr: #sprawdzamy, czy jest odpowiedz poprawna 
-        print( Fore.GREEN + 'Right!', Style.RESET_ALL + ' ') #kolorujemy tekst
+        print( Fore.GREEN + 'Right!', Style.RESET_ALL + ' ') 
         correctAns += 1 #dodajemy poprawną odpowiedz
     else:
-        print(Fore.RED + 'No.' , Style.RESET_ALL + 'the correct answer is', correctAnsversString) #kolorujemy tekst
+        print(Fore.RED + 'No.' , Style.RESET_ALL + 'the correct answer is', correctAnsversString) 
         wrongAns += 1 #dodajemy niepoprawną odpowiedz
-    if counter == 30: #ustaliamy po jakiej ilości zadanych pytań pokażą się wyniki
+    if counter == currentMaxQtyQstns: #ustaliamy po jakiej ilości zadanych pytań pokażą się wyniki
         break
 pers = (correctAns / qstnsQty) * 100  #procent poprawnych odpowiedzi  
 print('quantity of answers ',qstnsQty)
